1f0b5502593e18ac05e1be334a9c74aaf6d4c78c
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-14 20:26:42 -0700

    updated queries.sql

--- queries.sql
+++ queries.sql
@@ -11,3 +11,6 @@ SELECT * FROM TITLE_AUTHORS;
 
 -- 2) Find all the info on authors whose last name starts with 'H.'
 SELECT * FROM AUTHORS where AU_LNAME like 'H%';
+
+-- 3) Find the title, type salesprice, and published date for all books published between July 15,2014 and August 15, 2014
+SELECT TITLE_NAME, 'TYPE', PRICE, PUBDATE FROM TITLES WHERE PUBDATE BETWEEN '07/15/2014' and '08/15/2014';
