b06b9e3b64a7431d8f63e64f0b1fa8ec1437b134
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-14 20:29:17 -0700

    added question 4 solution

--- queries.sql
+++ queries.sql
@@ -12,5 +12,9 @@ SELECT * FROM TITLE_AUTHORS;
 -- 2) Find all the info on authors whose last name starts with 'H.'
 SELECT * FROM AUTHORS where AU_LNAME like 'H%';
 
--- 3) Find the title, type salesprice, and published date for all books published between July 15,2014 and August 15, 2014
-SELECT TITLE_NAME, 'TYPE', PRICE, PUBDATE FROM TITLES WHERE PUBDATE BETWEEN '07/15/2014' and '08/15/2014';
+-- 3) Find the title, type, salesprice, published date for all titles published between July 15, 2014 and August 15, 2014
+SELECT TITLE_NAME, "TYPE", PRICE, PUBDATE From TITLES
+where PUBDATE between '07/15/2014' and '08/15/2014';
+
+-- 4) Find all the information for titles T01, T04, and T07
+SELECT * FROM TITLES WHERE TITLE_ID in ('T01', 'T04', 'T07');
